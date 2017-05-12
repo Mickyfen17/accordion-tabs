903d7987e4f4bf59e88a489665e2c3f1d7db5cfa
Michael Fenwick <mickyfen17@aol.com>
2017-05-12 14:39:24 -0600

    toggle header and article classes for when selected

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const header = document.getElementById('header-1');
 const headers = document.querySelectorAll('h1');
 
 headers.forEach((header, i) => {
   header.addEventListener('click', function(){
-    if(this.nextElementSibling.classList.contains('show')) {
-      this.nextElementSibling.classList.add('show')
-    }
-    tabsShown()
-    this.nextElementSibling.classList.add('show')
-    // const tabArticle = this.nextElementSibling;
-    // if(tabArticle.classList.contains('show')) {
-    //   tabArticle.classList.remove('show')
-    // } else {
-    //   tabsShown()
-    //   tabArticle.classList.add('show')
+    // if(this.nextElementSibling.classList.contains('show')) {
+    //   this.nextElementSibling.classList.add('show')
     // }
+    // tabsShown()
+    // this.nextElementSibling.classList.add('show')
+    const tabArticle = this.nextElementSibling;
+    if(tabArticle.classList.contains('show-tab')) {
+      tabArticle.classList.remove('show-tab');
+      this.classList.remove('show-header');
+    } else {
+      tabsShown()
+      tabArticle.classList.add('show-tab');
+      this.classList.add('show-header');
+
+    }
   })
 })
 
-const tabsShown = () => {
+const tabsShown = function(){
   const tabs = document.querySelectorAll('article');
   tabs.forEach(tab => {
-    if(tab.classList.contains('show')) {
-      tab.classList.remove('show')
+    if(tab.classList.contains('show-tab')) {
+      tab.classList.remove('show-tab');
+      tab.previousElementSibling.classList.remove('show-header');
     }
   })
 }
