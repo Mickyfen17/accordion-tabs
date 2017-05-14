e7637564866bf8bfa06a3b1fd57633f19ad790d4
Michael Fenwick <mickyfen17@aol.com>
2017-05-13 19:16:06 -0600

    add onload func to show first tab above 500px

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-const headers = document.querySelectorAll('h1');
+const headers = document.querySelectorAll('.tab-header');
 
 headers.forEach((header, i) => {
   header.addEventListener('click', function(){
-    // if(this.nextElementSibling.classList.contains('show')) {
-    //   this.nextElementSibling.classList.add('show')
-    // }
-    // tabsShown()
-    // this.nextElementSibling.classList.add('show')
     const tabArticle = this.nextElementSibling;
-    if(tabArticle.classList.contains('show-tab')) {
+    if (tabArticle.classList.contains('show-tab')) {
       tabArticle.classList.remove('show-tab');
       this.classList.remove('show-header');
     } else {
       tabsShown()
       tabArticle.classList.add('show-tab');
       this.classList.add('show-header');
-
     }
   })
 })
 
 const tabsShown = function(){
-  const tabs = document.querySelectorAll('article');
+  const tabs = document.querySelectorAll('.tabs');
   tabs.forEach(tab => {
-    if(tab.classList.contains('show-tab')) {
+    if (tab.classList.contains('show-tab')) {
       tab.classList.remove('show-tab');
       tab.previousElementSibling.classList.remove('show-header');
     }
   })
 }
+
+window.onload = () => {
+  if (window.innerWidth > 500) {
+    document.getElementById('tab1').classList.add('show-tab');
+    document.querySelector('.tab-header').classList.add('show-header');
+  }
+}
+
+
